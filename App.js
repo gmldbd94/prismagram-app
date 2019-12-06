f0f1d0d2843fc8c0db9ac7570c0ab70f241b879c
gmldbd94 <gmldbd@likelion.org>
2019-12-07 03:16:25 +0900

    Feed View End

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import styles from "./styles";
 import NavController from "./components/NavController";
 import { AuthProvider } from "./AuthContext";
 
-AsyncStorage.clear()
+//AsyncStorage.clear()
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
