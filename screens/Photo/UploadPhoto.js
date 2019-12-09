eb219d11c3a8c6dabc17f378f7236d06e3b2d9dc
gmldbd94 <gmldbd@likelion.org>
2019-12-09 14:10:47 +0900

    error uploadMutation

--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -2,13 +2,25 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Image, ActivityIndicator, Alert } from "react-native";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
 import useInput from "../../hooks/useInput";
 import styles from "../../styles";
 import constants from "../../constants";
-import AuthButton from "../../components/AuthButton";
-
+import { useMutation } from "react-apollo-hooks";
+import { FEED_QUERY } from "../Tabs/Home";
+import Array from "react";
 //안드로이드 방식과 ios 방식을 구별해줘야 한다.
 import { Platform } from "react-native";
+
+const UPLOAD = gql`
+  mutation upload($caption: String!, $files: [String!]!, $location: String) {
+    upload(caption: $caption, files: $files, location: $location) {
+      id
+      caption
+      location
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -48,10 +60,20 @@ const Text = styled.Text`
 
 export default ({ navigation }) => {
   const [loading, setIsLoading] = useState(false);
-  const [fileUrl, setFileUrl] = useState("");
+  // const [fileUrl, setfileUrl] = useState("");
   const photo = navigation.getParam("photo");
-  const captionInput = useInput("dfdf");
-  const locationInput = useInput("dfdfd");
+  const captionInput = useInput("");
+  const locationInput = useInput("");
+  // const uploadMutation = useMutation(UPLOAD, {
+  //   variables: {
+  //     caption: captionInput.value,
+  //     location: locationInput.value,
+  //     files: [fileUrl]
+  //   }
+  // });
+  const uploadMutation = useMutation(UPLOAD, {
+    refetchQueries: () => [{ query: FEED_QUERY }]
+  });
   const handleSubmit = async () => {
     if (captionInput.value === "" || locationInput.value === "") {
       Alert.alert("All fields are required");
@@ -60,10 +82,9 @@ export default ({ navigation }) => {
     const name = photo.filename;
     const [, type] = name.split(".");
     if(Platform.OS === "android"){
-      console.log(type.toLowerCase());
       formData.append("file", {
         name,
-        type: "image/jpeg",
+        type: "image/"+type.toLowerCase(),
         uri: photo.uri
       });
     }
@@ -75,10 +96,10 @@ export default ({ navigation }) => {
       });
     }
 
-    console.log(formData);
     //192.168.43.92
     try {
       setIsLoading(true);
+      console.log("start");
       const{
         data: { location }
       } =
@@ -87,21 +108,20 @@ export default ({ navigation }) => {
             "content-type": "multipart/form-data"
           }
       });
-      setFilUrl(location);
-      // const{
-      //   data: { location }
-      // } = await axios.post("http://192.168.43.92:4000", formData, {
-      //   headers: {
-      //     "content-type": "multipart/form-data"
-      //   }
-      // });
-      // .then(function (response) {
-      // console.log(response);
-      // })
-      // .catch(function (error) {
-      //   console.log(error);
-      // });
-      // setFileUrl(location);
+      console.log(location);
+      // setfileUrl(location);
+      const {
+        data: { upload }
+      } = await uploadMutation({
+        variables: {
+          files: [location],
+          caption: captionInput.value,
+          location: locationInput.value
+        }
+      });
+      if (upload.id) {
+        navigation.navigate("TabNavigation");
+      }
     } catch (e) {
       console.log(e);
       Alert.alert("Can't upload", "Try later");
--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -8,7 +8,7 @@ import Post from "../../components/Post";
 import { POST_FRAGMENT } from "../../fragments";
 
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     seeFeed {
       ...PostParts
